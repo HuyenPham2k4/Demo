b027712851f28894cd2fa1d54bc251b9bc60416e
HuyenPham2k4 <huyenpmpp03254@gmail.com>
2024-07-13 09:16:34 +0700

    sửa

--- src/DAO/nhanhVienDAO.java
+++ src/DAO/nhanhVienDAO.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package dam1;
+package DAO;
 
 /**
  *
- * @author Admin
+ * @author huyen
  */
-public class NewClass {
+public class nhanhVienDAO {
     
 }
